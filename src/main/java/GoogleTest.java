93e3ced8cdff85d1dbfb697aa35dfbf374813183
Gabriel Nascimento <gabriel.nascimento@construo.ch>
2022-07-10 10:49:37 +0100

    create folder .gitignore

--- src/main/java/GoogleTest.java
+++ src/main/java/GoogleTest.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class GoogleTest {
+import com.codeborne.selenide.CollectionCondition;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleTest {
+    @Test
+    public void googleSearchTest() {
+        open("https://google.com");
+        $(By.id("L2AGLb")).click();
+        $(By.name("q")).setValue("Naveen AutomationLabs");
+        $(By.name("btnK")).click();
+        $(By.id("logo")).shouldHave(appear);
+        String header = ($(By.xpath("//h3[text()='Welcome to Naveen AutomationLabs - naveen automationlabs']"))).getText();
+        System.out.println(header);
+        Assert.assertEquals(header, "Welcome to Naveen AutomationLabs - naveen automationlabs");
+        int headerCount = $$(By.cssSelector(".LC20lb.MBeuO.DKV0Md")).size();
+        System.out.println(headerCount);
+        $$(By.cssSelector(".LC20lb.MBeuO.DKV0Md")).shouldHave(CollectionCondition.size(9));
+
+    }
 }
--- src/main/java/LaunchBrowser.java
+++ src/main/java/LaunchBrowser.java
@@ -1,8 +1,34 @@
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LaunchBrownser {
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.*;
+
+public class LaunchBrowser {
 
     @Test
-    public void
+    public void launchBrowserTest() {
+        //by default use google Chrome
+//        Configuration.browser = "microsoft edge";
+//        System.setProperty("selenide.browser","chrome");
+        Configuration.headless = true; //by default headless is false
+       // Configuration.startMaximized = true;
+        //Configuration.baseUrl("https://google.com");
+        Configuration.webdriverLogsEnabled = true;
+        open("https://google.com");
+        $(By.id("L2AGLb")).click();
+        $(By.name("q")).setValue("Naveen AutomationLabs");
+        $(By.name("btnK")).click();
+        $(By.id("logo")).shouldHave(appear);
+        String header = ($(By.xpath("//h3[text()='Welcome to Naveen AutomationLabs - naveen automationlabs']"))).getText();
+        System.out.println(header);
+        Assert.assertEquals(header, "Welcome to Naveen AutomationLabs - naveen automationlabs");
+        int headerCount = $$(By.cssSelector(".LC20lb.MBeuO.DKV0Md")).size();
+        System.out.println(headerCount);
+        $$(By.cssSelector(".LC20lb.MBeuO.DKV0Md")).shouldHave(CollectionCondition.size(9));
+    }
 
 }
